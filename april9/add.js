1420f91e0291f6da54c99d95bec8fc8dbd3136f3
Dillon Ginsburg <dillon.ginsburg@gmail.com>
2019-04-09 09:02:14 -0600

    commit add function 4_9

---
+++ april9/add.js
@@ -0,0 +1,8 @@
+let number1=10
+let number2=5
+
+function add (number1, number2) {
+	return number1 + number2;
+}
+
+add(number1, number2)
---
+++ april9/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+	<head>
+		<meta charset="UTF-8" />
+		<title>snap challenge math function</title>
+		<link ref="script" href="add.js"
+	</head>
+
+	<body>
+		<p>
+		</p>
+	</body>
+</html>
