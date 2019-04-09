5203b3acfcd694a009beaff850dc61472e7e7828
Dillon Ginsburg <dillon.ginsburg@gmail.com>
2019-04-09 14:18:10 -0600

    commit add function 4_9

--- april9/add.js
+++ april9/add.js
@@ -6,3 +6,20 @@ function add (number1, number2) {
 }
 
 add(number1, number2)
+
+console.log((add)(-2,5))
+
+
+/**
+ *Take two names and concatenate them.
+ *
+ * @param string firstName The given name to concatenate.
+ * @pram string lastName The family name to concatenate
+ * @return string the full name of our person.
+ */
+
+function getFullName(firstName, lastName) {
+	return firstName + lastName;
+}
+
+console.log(getFullName("Paul", "Sschulzetenberg"));
