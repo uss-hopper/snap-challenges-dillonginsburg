d4cb4070fc72534506a65efd20112780dc2c701f
Dillon Ginsburg <dillon.ginsburg@gmail.com>
2019-04-16 09:06:22 -0600

    commit snap 4-16

---
+++ snap-4-16/snap4-16.sql
@@ -0,0 +1,12 @@
+drop table if exists Task;
+
+create table Task (
+	taskId binary(16) not null,
+	taskTitle varchar(256) not null,
+	taskStartDate datetime,
+	taskDueDate datetime,
+	taskStatus varchar(64) not null,
+	taskPriority varchar(64) not null,
+	taskDescription varchar(256)
+		primary key(TaskId)
+);
