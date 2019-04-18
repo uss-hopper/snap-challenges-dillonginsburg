46ece75ad2eaff9796ebc5a182c3a6720029a23d
Dillon Ginsburg <dillon.ginsburg@gmail.com>
2019-04-18 09:04:47 -0600

    commit snap 4-18

---
+++ snap-4-18/snap-4-18.js
@@ -0,0 +1,25 @@
+i=VarInt
+
+function lbsToG (i * 453.59237) {
+	return i * 453.59237
+}
+
+function ozToG(i * 28.3495231) {
+	return i* 28.3495231
+}
+
+function kgToG(i * 1000) {
+	return i * 1000
+}
+
+function mgToG(i * .001) {
+	return i * .001
+}
+
+If input in lbs, function lbsToG
+
+If input in oz, function ozToG
+
+If input in kg, function kgToG
+
+if input in mg, function mgToG
